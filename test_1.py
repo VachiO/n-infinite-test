31f872e56d0334a79db0b6d95db8fe4f052ce547
VachiO <104642216+VachiO@users.noreply.github.com>
2024-11-20 20:25:57 +0700

    Add files via upload

---
+++ test_1.py
@@ -0,0 +1,24 @@
+def accum(s: str):
+    # Write your code here.
+
+    def get_apb(index, alphabet):
+        return alphabet.upper() + (alphabet.lower() * index)
+
+    len_s = len(s)
+    result_before_merge = []
+
+    for i in range(len_s):
+        text = get_apb(i, s[i])
+        result_before_merge.append(text)
+
+    result = "-".join(result_before_merge)
+
+    return result
+
+
+# Run this file for test
+assert accum("ZpglnRxqenU") == "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu"
+assert accum("NyffsGeyylB") == "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb"
+assert accum("MjtkuBovqrU") == "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu"
+assert accum("EvidjUnokmM") == "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm"
+assert accum("HbideVbxncC") == "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc"
